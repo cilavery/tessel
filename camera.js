05fb7ab775376668c242b07a6a3e02f002b9aa56
Cecilia Avery <cilsong@gmail.com>
2018-03-19 15:35:53 -0400

    first commit

---
+++ camera.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const os = require('os');
+const path = require('path');
+const port = 8888;
+
+const av = require('tessel-av');
+const camera = new av.Camera({
+  width: 320,
+  height: 240,
+});
+
+server.listen(port, function () {
+  console.log(`http://${os.hostname()}.local:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/stream', (request, response, next) => {
+  response.redirect(camera.url);
+});
